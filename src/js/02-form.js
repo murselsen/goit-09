23411b5f1b37be656b00606c1eae58aeda31027e
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-23 22:47:24 +0300

    ``` No code changes made; skipping commit. ```

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -22,8 +22,7 @@ const formDataLocal = JSON.parse(localStorage.getItem('feedback-form-state'));
 console.log(formDataLocal);
 console.log(typeof formDataLocal);
 
-form.elements.email.value = formDataLocal.email;
-
-/* form.addEventListener('submit', event => {
-  event.preventDefault();
-}); */
+if (formDataLocal !== null) {
+  form.elements.email.value = formDataLocal.email;
+  form.elements.message.value = formDataLocal.message;
+}
