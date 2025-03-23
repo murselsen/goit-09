c2247ee7ff60d67d899fd8b7b63c1b8b1084b3c0
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-23 22:20:40 +0300

    No code changes made; skipping commit.

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -3,28 +3,18 @@ const form = document.querySelector('.feedback-form');
 //input event
 form.addEventListener('input', event => {
   console.log(event);
+
+  
   const inputValue = event.target.value.trim();
   const inputName = event.target.name;
   // feedback-form-state
-  localStorage.setItem('feedback-form-state',
+  localStorage.setItem(
+    'feedback-form-state',
     JSON.stringify({
-      [inputName]: inputValue
+      [inputName]: inputValue,
     })
   );
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* form.addEventListener('submit', event => {
   event.preventDefault();
