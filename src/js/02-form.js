ee028b0b2590cac086a96550bf0ac7d79bbc3cfe
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-23 22:38:30 +0300

    Set email input value from localStorage feedback form data

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -22,7 +22,7 @@ const formDataLocal = JSON.parse(localStorage.getItem('feedback-form-state'));
 console.log(formDataLocal);
 console.log(typeof formDataLocal);
 
-form.elements.email.value = 
+form.elements.email.value = formDataLocal.email;
 
 /* form.addEventListener('submit', event => {
   event.preventDefault();
