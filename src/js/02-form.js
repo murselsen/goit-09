ed433a51e670693e04262609fe483eb7b450040b
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-23 22:30:19 +0300

    Update feedback form input event to log form elements and save email and message to localStorage

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -2,9 +2,6 @@ const form = document.querySelector('.feedback-form');
 
 //input event
 form.addEventListener('input', event => {
-  // console.log(event);
-
-  console.log(form.elements);
   const email = form.elements.email.value;
   const message = form.elements.message.value;
   console.log({
@@ -12,8 +9,19 @@ form.addEventListener('input', event => {
     message: message,
   });
   // feedback-form-state
-  // localStorage.setItem('feedback-form-state', JSON.stringify({}));
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      email: email,
+      message: message,
+    })
+  );
 });
+
+const formDataLocal = 
+
+
+
 
 /* form.addEventListener('submit', event => {
   event.preventDefault();
